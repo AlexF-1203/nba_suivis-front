c73ff657cffc077fd82b48f0160769920c16c821
Alexis Ferrere <alexferrere@outlook.fr>
2025-01-13 18:54:24 +0100

    done

--- App.jsx
+++ App.jsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>NBA Suivis App</Text>
     </View>
   );
 }
